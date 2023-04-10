dada06217035b2c5f6d795734c3a7c61c2b8b378
Jhonni10 <jhonieripianoscue@gmail.com>
2023-04-10 15:00:25 -0500

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 // import Contador from './components/Contador';
-import Card from './components/Cards';
+import Cards from './components/Card';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" />
         <Contador inicial={0} factor={2}></Contador> */}
-        <Card></Card>
+        <Cards></Cards>
       </header>
     </div>
   );
